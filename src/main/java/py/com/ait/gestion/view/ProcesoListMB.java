ab1edfbda4fc66f634ce4b257e442a20baf6afaa
Hector Martinez <hdmartinez@pol.una.py>
2013-10-10 19:38:16 -0300

    Fix orden de aparicion de observaciones al agregar
    Al darle nueva observacion, la ultima observacion sale en el fondo,
    debe salir ordenado como cuando se abre el popup (descendente por
    fecha de creacion)

--- src/main/java/py/com/ait/gestion/view/ProcesoListMB.java
+++ src/main/java/py/com/ait/gestion/view/ProcesoListMB.java
@@ -7,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -987,7 +988,10 @@ public class ProcesoListMB extends AbstractListPageBean<Proceso, Long> {
 
 			observacionBC.registrar(obs);
 			if (observaciones != null){
-				observaciones.add(obs);
+				List<Observacion> nuevasObservaciones = new ArrayList<Observacion>();
+				nuevasObservaciones.add(obs);
+				nuevasObservaciones.addAll(observaciones);
+				observaciones = nuevasObservaciones;
 			}
 			agregarMensaje("Observacion creada");
 			this.setDescripcionObsP("");
