0a9a854299a8a163ebae1b6986d38fc7f7bd6d90
Hector Martinez <hdmartinez@pol.una.py>
2015-02-20 20:27:55 -0300

    Script para eliminar datos de tablas de gestion

---
+++ delete_all.sql
@@ -0,0 +1,8 @@
+delete from actividad_checlikst_detalle;
+delete from actividad;
+delete from documento_rol;
+delete from documento;
+delete from log_sesion;
+delete from notificaciones;
+delete from observacion;
+delete from proceso;
